3560d970b93a9b67c64b994a0af094e06ee5fa19
giosurmanidze <giorgisurmanidze917@gmail.com>
2024-03-20 23:30:09 +0400

    feat/role seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,50 +5,21 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
-use Spatie\Permission\Models\Role;
-use Spatie\Permission\Models\Permission;
 
 class DatabaseSeeder extends Seeder
 {
-
-    /**
-     * List of applications to add.
-     */
-    private $permissions = [
-        'role-list',
-        'role-create',
-        'role-edit',
-        'role-delete',
-        'product-list',
-        'product-create',
-        'product-edit',
-        'product-delete'
-    ];
-
-
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        foreach ($this->permissions as $permission) {
-            Permission::create(['name' => $permission]);
-        }
+        // User::factory(10)->create();
 
-        // Create admin User and assign the role to him.
-        $user = User::create([
-            'name' => 'Prevail Ejimadu',
-            'email' => 'test@example.com',
-            'password' => Hash::make('password')
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
 
-        $role = Role::create(['name' => 'Admin']);
-
-        $permissions = Permission::pluck('id', 'id')->all();
-
-        $role->syncPermissions($permissions);
-
-        $user->assignRole([$role->id]);
+        $this->call(RoleSeeder::class);
     }
 }
