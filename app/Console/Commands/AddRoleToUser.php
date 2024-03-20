df111bf5db71f9d33896f8bcab50c75d36b5b67c
giosurmanidze <giorgisurmanidze917@gmail.com>
2024-03-20 23:45:07 +0400

    feat/role model and custom add role command

---
+++ app/Console/Commands/AddRoleToUser.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use App\Models\Role;
+use Illuminate\Console\Command;
+
+class AddRoleToUser extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'role:add {email : The email address of the user} {role : The role name}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Add a role to a user';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $email = $this->argument('email');
+        $roleName = $this->argument('role');
+
+        $user = User::where('email', $email)->first();
+        $role = Role::where('name', $roleName)->first();
+
+        if ($user && $role) {
+            $user->role_id = $role->id;
+            $user->save();
+            $this->info("Role '$roleName' added to user with email '$email'");
+        } else {
+            $this->error("User or role not found");
+        }
+    }
+}
