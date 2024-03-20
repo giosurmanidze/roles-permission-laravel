3b7624a7ba00d94272e2bd607ff641dc3ddfdded
giosurmanidze <giorgisurmanidze917@gmail.com>
2024-03-20 23:32:19 +0400

    feat/define middleware

---
+++ app/Http/Middleware/EnsureUserRole.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class EnsureUserRole
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next, ...$roles)
+    {
+        if (!in_array($request->user()->role->name, $roles)) {
+            // Redirect or abort if the user role is not allowed
+            return abort(403, 'Unauthorized action.');
+        }
+
+        return $next($request);
+    }
+}
